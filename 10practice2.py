86222a4333a7ccba15af28f2cfa659af8f4565a8
ekanshbhardwaj-01 <gojosaturo2734@gmail.com>
2026-07-06 18:40:50 +0530

    elif practice

---
+++ 10practice2.py
@@ -0,0 +1,13 @@
+num=int(input("enter a number :"))
+if(num==0):
+    print(num ,"is neither odd nor even")
+
+elif((num%2)==0):
+     print(num,"is Even")
+else:
+     print(num," is Odd")
+
+if((num%7)==0):
+     print(num, "is divisible by 7")
+else:
+     print(num, "is not divisible by 7 ")
